03c60db4d50522d8da153687509f96058a86391a
Goncalo Vilhena <vilhena@gmail.com>
2011-10-15 10:25:50 -0300

    form

---
+++ src/Trabalho-Final/Parte1/JavascriptEjQuery/js/movies.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../../../Scripts/jquery-1.6.4.js" />
+
+
+$(function () {
+    $("#list").click(function () {
+        $("body").fadeOut(function () {
+            $("#css").attr("href", "css/movieslist.css");
+            $("body").fadeIn();
+        });
+    });
+
+    $("#grid").click(function () {
+        $("body").fadeOut(function () {
+            $("#css").attr("href", "css/moviesgrid.css");
+            $("body").fadeIn();
+        });
+    });
+
+    $("#clean").click(function () {
+        $("body").fadeOut(function () {
+            $("#css").attr("href", "");
+            $("body").fadeIn();
+        });
+    });
+
+});
